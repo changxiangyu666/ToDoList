ab894b48cf944a480266b13acb66da2eb53953ac
changxiangyu <1792286994@qq.com>
2020-04-14 23:33:25 +0800

    Default Changelist

---
+++ app/webpack.config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by MrsChangXY on 2020/4/14.
+ */
+var webpack=require('webpack');
+module.exports = {
+    entry: "./runapp.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" }
+        ]
+    },
+    plugins:[
+        new webpack.BannerPlugin('²ËÄñ½Ì³Ì webpack ÊµÀý')
+    ]
+};
